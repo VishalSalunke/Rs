0c25fb428f76ba1496cd9d1d7c69b15c927d101d
VishalSalunke <salunkevishal05@gmail.com>
2021-12-01 09:57:37 +0530

    adding calculator

---
+++ HTML-CSS/calculator/calc.js
@@ -0,0 +1,30 @@
+const inputBox = document.querySelector("#calc-input")
+
+function addInput(inputValue){
+  const currentInput = inputBox.value
+  const operations = ['*', '/', '+', '-']
+  const lastChar = currentInput[currentInput.length - 1]
+  let isOperationExists = operations.includes(lastChar) && operations.includes(inputValue)
+
+  if(isOperationExists){
+    return
+  }else{
+    isOperationExists = false
+  }
+  const newValue = currentInput + inputValue
+  inputBox.value = newValue
+}
+
+function result(){
+
+  try {
+    inputBox.value = eval(inputBox.value)
+  } catch (error) {
+    alert('please enter valid operation')
+    inputBox.value = ''
+  }
+}
+
+function allClear(){
+  inputBox.value = ''
+}
