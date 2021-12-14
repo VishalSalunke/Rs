82beab6939c84e5750d8ed2998459d4b5b5671d3
VishalSalunke <salunkevishal05@gmail.com>
2021-12-14 22:28:27 +0530

    js

---
+++ HTML-CSS/Js/main.js
@@ -0,0 +1,50 @@
+console.log('Hello Javascript')
+
+
+// string
+// number
+// boolean
+// object
+// undefined
+// null
+/* */
+
+let firstName = 'vishal'
+let age = 29
+let isSelected = true
+
+let person = {
+  firstName: 'vishal',
+  age: 29,
+  isSelected: true
+}
+
+console.log(firstName)
+console.log(age)
+console.log(isSelected)
+console.log(person)
+
+function addition(a,b) {
+ let sum = a + b
+  console.log(sum)
+  if(a==='2'){
+    let c = 4
+    console.log('hello')
+  }else{
+    console.log('bye')
+  }
+  return sum
+}
+addition(2,5)
+console.log(sum)
+// let sum = addition(2,5)
+
+
+
+
+
+
+
+
+
+
